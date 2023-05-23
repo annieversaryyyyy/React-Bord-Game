a157e652b999cb31f1b65454442b4121c7e53131
anna.somik <kduzkdkuzkdkd@gmail.com>
2023-05-23 21:29:42 +0600

    Убраны лишние пробелы

--- src/App.js
+++ src/App.js
@@ -4,13 +4,12 @@ import './Block/Block.css';
 
 const App = () => {
     const [blocks, setBlocks] = useState([]);
-    const [win, setWin] = useState(false);
     const [count,setCount] = useState(0);
 
     const makeBlocks = () => {
         const newArray = [];
 
-        for(let i = 0; i < 36; i++) {
+        for (let i = 0; i < 36; i++) {
             newArray.push({hasItem: false, id: i,class: "block"});
         }
 
@@ -18,29 +17,24 @@ const App = () => {
         setBlocks(newArray);
     };
 
-    if (blocks.length === 0){
+    if (blocks.length === 0) {
        makeBlocks();
     }
 
     const onCheckItem = (id) => {
             setCount(count + 1);
             if(blocks[id].hasItem === true) {
-                blocks[id].class = "circle"
-                alert("Congratulation!You find circle!")
+                blocks[id].class = "circle";
+                alert("Congratulation!You find circle!");
             }else{
                 blocks[id].class = "show";
             }
            setBlocks([...blocks]);
-
     };
-
 
     const onResetGame = () => {
         makeBlocks();
-        setWin(false);
     };
-
-
 
     return (
       <div className="container" >
@@ -55,7 +49,7 @@ const App = () => {
                   ))
               }
           </div>
-          <p className="tries"  on>Tries:</p>
+          <p className="tries">Tries:</p>
           <div className="playground"> <button className="resetBtn" onClick={onResetGame}>Reset</button></div>
       </div>
     );
