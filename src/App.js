28d08013b2438c3081cb339e7c44fd020c4ac60d
anna.somik <kduzkdkuzkdkd@gmail.com>
2025-08-06 17:06:30 +0600

    feat: add tries counter

--- src/App.js
+++ src/App.js
@@ -1,58 +1,65 @@
-import {useState} from "react";
+import { useState, useEffect } from "react";
 import Block from "./Block/Block";
-import './Block/Block.css';
+import "./Block/Block.css";
 
 const App = () => {
-    const [blocks, setBlocks] = useState([]);
-    const [count,setCount] = useState(0);
+  const [blocks, setBlocks] = useState([]);
+  const [count, setCount] = useState(0);
 
-    const makeBlocks = () => {
-        const newArray = [];
+  const makeBlocks = () => {
+    const newArray = [];
 
-        for (let i = 0; i < 36; i++) {
-            newArray.push({hasItem: false, id: i,class: "block"});
-        }
-
-        newArray[Math.floor( Math.random() * 36) + 1].hasItem = true;
-        setBlocks(newArray);
-    };
-
-    if (blocks.length === 0) {
-       makeBlocks();
+    for (let i = 0; i < 36; i++) {
+      newArray.push({ hasItem: false, id: i, class: "block" });
     }
 
-    const onCheckItem = (id) => {
-            setCount(count + 1);
-            if(blocks[id].hasItem === true) {
-                blocks[id].class = "circle";
-                alert("Congratulation!You find circle!");
-            }else{
-                blocks[id].class = "show";
-            }
-           setBlocks([...blocks]);
-    };
+    newArray[Math.floor(Math.random() * 36) + 1].hasItem = true;
+    setBlocks(newArray);
+  };
 
-    const onResetGame = () => {
-        makeBlocks();
-    };
+  useEffect(() => {
+    makeBlocks();
+  }, []);
 
-    return (
-      <div className="container" >
-          <div className="playgroundBlock">
-              {
-                  blocks.map(block => (
-                      <Block
-                          key={block.id}
-                          class={block.class}
-                          onHeandlerBlock={() => onCheckItem(block.id)}
-                      />
-                  ))
-              }
-          </div>
-          <p className="tries">Tries:</p>
-          <div className="playground"> <button className="resetBtn" onClick={onResetGame}>Reset</button></div>
+  const onCheckItem = (id) => {
+    setCount((prevCount) => prevCount + 1);
+    if (blocks[id].hasItem === true) {
+      blocks[id].class = "circle";
+      alert("Congratulation!You find circle!");
+      setTimeout(() => {
+        onResetGame();
+      }, 500); 
+    } else {
+      blocks[id].class = "show";
+    }
+    setBlocks([...blocks]);
+  };
+
+  const onResetGame = () => {
+    makeBlocks();
+    setCount(0);
+  };
+
+  return (
+    <div className="container">
+      <div className="playgroundBlock">
+        {blocks.map((block) => (
+          <Block
+            key={block.id}
+            class={block.class}
+            onHeandlerBlock={() => onCheckItem(block.id)}
+          />
+        ))}
       </div>
-    );
+      <p className="tries">Tries:{count}</p>
+      <div className="playground">
+        {" "}
+        <button className="resetBtn" onClick={onResetGame}>
+          Reset
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default App;
